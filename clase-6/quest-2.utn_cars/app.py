715747487aeebed23b2af2c42b1a6161701d9f42
Nazareno Sotelo <nazareno.sotelo19@gmail.com>
2025-09-12 14:00:22 -0300

    snd_funcion

--- clase-6/quest-2.utn_cars/app.py
+++ clase-6/quest-2.utn_cars/app.py
@@ -1,5 +1,14 @@
+import funciones_nivel_1 as lvl1
+from utn_fra.datasets import (
+        lista_autos_marcas,
+        lista_autos_modelos,
+        lista_autos_cantidades,
+        lista_autos_precios
+    )
 
-def elegir_opcion()->None:
+
+
+def elegir_opcion()->int|None:
     """
     Eligue la opcion que el usuario ingresa
 
@@ -13,4 +22,8 @@ def elegir_opcion()->None:
 
     match opcion_str:
         case "1":
+            lvl1.imprimir_modelos(lista_autos_modelos)
+        case "2":
+            lvl1.imprimir_modelo_y_cantidad(lista_autos_modelos, lista_autos_cantidades)
+        case "3":
             
--- clase-6/quest-2.utn_cars/funciones_nivel_1.py
+++ clase-6/quest-2.utn_cars/funciones_nivel_1.py
@@ -7,5 +7,44 @@ from utn_fra.datasets import (
 
 
 # Recorrer la lista imprimiendo por consola el modelo de autos que hay en cada garage
+def imprimir_modelos(lista_modelos:list)->None:
+    """
+    Muestra por consola los modelos de auto que hay por cada garage
+    
+    :params: lista_modelos(list) = La lista de modelos de autos
+    
+    :returns: 
+    Imprime la cantidad de modelos de autos
+    """
+    
+    for indice in range(len(lista_modelos)):
+        modelo_actual = lista_modelos[indice]
+        mensaje = f"En el garage N°{indice + 1} esta el modelo {modelo_actual}"
+        print(mensaje)
 
-def imprimir_modelos(lista_modelos)
+
+# Recorrer la lista imprimiendo por consola el modelo de autos que hay en cada garage 
+# junto con la cantidad que posee.
+
+def imprimir_modelo_y_cantidad(lista_modelo:list, lista_cantidaes:list)->None:
+    """
+    Muestra por consola el modelo de auto de cada garage con su cantidad
+    
+    :params: lista_modelos(list) = La lista de modelos de autos
+    :params: lista_cantidades(list) = La lista de cantidades de autos
+    
+    :returns:
+    Devuelve por consola el modelo del auto con su cantidad
+    """
+    
+    for indice in range(len(lista_modelo)):
+        modelo_actual = lista_modelo[indice]
+        cantidad_actual = lista_cantidaes[indice]
+        mensaje = f"Garage N°{indice + 1}: modelo - {modelo_actual} | cantidad {cantidad_actual}"
+        print(mensaje)
+        
+
+# Recorrer las listas y determinar cuál es el modelo de auto que más cantidad 
+# posee la concesionaria (MÁXIMO).
+
+def 
